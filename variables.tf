8a53ec7ebabcf7c4c64c1f0cd35ec7562c57d2db
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-03-24 21:15:47 +0100

    Add types to all variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,23 @@
 variable "parent_domain" {
+  type        = string
   description = "The parent domain to delegate from. Must already exist on DigitalOcean, managed by the same account"
 }
 
 variable "label" {
+  type        = string
   description = "DNS label of the subdomain to create"
 }
 
 variable "glue_record_ttl" {
+  type        = number
   description = "TTL (in seconds) of the glue records"
   default     = 3600
 }
 
 variable "nameservers" {
-  default = [
+  type        = list(string)
+  description = "List of nameservers for delegation"
+  default     = [
     "ns1.digitalocean.com.",
     "ns2.digitalocean.com.",
     "ns3.digitalocean.com.",
